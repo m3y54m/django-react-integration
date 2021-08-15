0448774a3cdc611b34d9929b1d5e4fc722db7100
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2021-08-15 05:29:38 +0430

    -- FIX ROOT URL --

--- backend/urls.py
+++ backend/urls.py
@@ -14,16 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.db import router
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 from rest_framework import routers
 from todo import views
+
+from django.views.generic import TemplateView
 
 router = routers.DefaultRouter()
 router.register(r'todos', views.TodoView, 'todo')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    re_path('.*', TemplateView.as_view(template_name='index.html')),
 ]
